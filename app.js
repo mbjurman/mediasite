affb86f39d1efdc61d8da7aba55933a1a3767463
Marcus Bjurman <mbjurman@gmail.com>
2014-01-12 23:11:35 +0100

    setup basic express.js app

--- app.js
+++ app.js
@@ -2,12 +2,18 @@
 var express = require('express');
 var app = express();
 
+app.use("/cms", express.static(__dirname + '/cms'));
+
 app.get('/', function(req, res){
   var body = 'Hello World';
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', Buffer.byteLength(body));
   res.end(body);
 });
+
+
+
+
 app.listen(8080);
 
 /*
